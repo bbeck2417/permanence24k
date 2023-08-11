297fd7b51cc5c867243b31a999e63306d3fef308
Billy Beck <billys.beck@gmail.com>
2023-08-11 17:15:15 -0400

    InitialBuild
    Initial Build

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ig from './instagram.png';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div className='font-face-tangerine'>
+            <div>
+                  <div className='aboutMe'>
+                      <h2>About Me</h2>
+                  </div>
+                  <div className='poet'>
+                      <h4>I am a poet thru Nature</h4>
+                  </div>
+            </div>  
+
+            <div className='content'>
+
+                <p>I enjoy traveling accross the world in observance of life. Looking for nature peaking through, in an abudance of interactions. Interactions only presented in that specific moment of time and in of place. For me to capture a moment of feeling and thought...</p>
+
+            </div>
+      
+          </div>
+          <div className='ig'>
+            <div>
+            <a href="https://www.instagram.com/will_iambeck" rel='noreferrer' target="_blank">
+              <img src={ig} alt="Poet vs Nature" />
+            </a>  
+            </div>
+          </div>
       </header>
     </div>
   );
