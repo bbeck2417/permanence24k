8acdd884ea796b029bea2718c15c71ef7296e52d
Billy Beck <billys.beck@gmail.com>
2023-08-15 15:00:56 -0400

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import React from 'react';
-import './App.css';
-import EnglishPeaks from './Components/englishPeaks';
-
+import React from "react";
+import "./App.css";
+import EnglishPeaks from "./Components/englishPeaks";
 
 function App() {
   return (
-      <div className="App App-header">
-      <div className='fade-component'>
-      
-      <EnglishPeaks/>
-      
+    <div className="App App-header">
+      <div className="fade-component">
+        <EnglishPeaks />
       </div>
-          
-      </div>
+    </div>
   );
 }
 
